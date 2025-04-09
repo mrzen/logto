0dcac5c061130cf3383638e4b4c30008e4403986
Leo Adamek <leo@adamek.me>
2025-04-09 10:13:48 +0000

    fix(console): remove unnecessary change to `uriValidator`

--- packages/console/src/utils/validator.ts
+++ packages/console/src/utils/validator.ts
@@ -1,7 +1,7 @@
 export const uriValidator = (value: string) => {
   try {
     // eslint-disable-next-line no-new
-    new URL(value.replaceAll('*', ''));
+    new URL(value);
   } catch {
     return false;
   }
